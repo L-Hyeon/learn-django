612c7f5bef1b72025b896aeb853efc2e1814e4cd
K <dkh1001@outlook.kr>
2022-02-04 22:50:03 +0900

    Modify Polls App

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 def index(request):
-  return HttpResponse("Hello Django World")
+  if (request.method == "GET"):
+    return HttpResponse("Hello Django World")
+
+  data = json.loads(request.body)
+  return HttpResponse(data["name"])
